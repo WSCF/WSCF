aa253f065c50e72d575d42b3a9f7441eade38235
alexmg <alex.meyergleaves@gmail.com>
2014-05-28 00:23:59 +1000

    Version number incremented to 1.0.14.

--- src/SolutionInfo.cs
+++ src/SolutionInfo.cs
@@ -29,8 +29,8 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.13.0")]
-[assembly: AssemblyFileVersion("1.0.13.0")]
+[assembly: AssemblyVersion("1.0.14.0")]
+[assembly: AssemblyFileVersion("1.0.14.0")]
 
 //
 // In order to sign your assembly you must specify a key to use. Refer to the 
